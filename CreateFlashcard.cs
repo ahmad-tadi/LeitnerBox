b5900a2690163658b3d0609ededcba09aff899ec
ahmad-tadi <ahmad.tadi@gmail.com>
2023-11-30 15:31:35 +0330

    Avoid existing question (repeatitive). Add questions end with add new q state)

--- CreateFlashcard.cs
+++ CreateFlashcard.cs
@@ -2,38 +2,47 @@
 
 class CreateFlashcard : State
 {
-		string? question;
-		string? answer;
-	public CreateFlashcard(LeitnerStateContext leitnerStateContext) : base(leitnerStateContext)
-	{
-		Console.WriteLine("Please enter the question or term to be learned:");
-	}
+    string? question;
+    string? answer;
+    public CreateFlashcard(LeitnerStateContext leitnerStateContext) : base(leitnerStateContext)
+    {
+        Console.WriteLine("Please enter the question or term to be learned:");
+    }
 
-	protected override void Command(string command)
-	{
-		if (question is null)
-		{
-			question = command;
-			SetState(this);
-		Console.WriteLine("Please enter the answer or definition to be recalled:");
-		}
-		else
-		{
-			answer = command;
-			var flashcard = new Flashcard
-			{
-				Question = question,
-				Answer = answer,
-				BoxNumber = 1,
-				LastReviewedDay = DateTime.Now.MapDateToNumber()
-			};
+    protected override void Command(string command)
+    {
+        if (question is null)
+        {
+            question = command;
 
-			_context.Flashcards.Add(flashcard);
+            var isExistingQuestion = _context.Flashcards.Any(f => f.Question == question);
+            if(isExistingQuestion)
+            {
+                Console.WriteLine("There is a question with this word.");
+                SetState<CreateFlashcard>();
+                return;
+            }
 
-			_context.SaveChanges();
+            SetState(this);
+            Console.WriteLine("Please enter the answer or definition to be recalled:");
+        }
+        else
+        {
+            answer = command;
+            var flashcard = new Flashcard
+            {
+                Question = question,
+                Answer = answer,
+                BoxNumber = 1,
+                LastReviewedDay = DateTime.Now.MapDateToNumber()
+            };
 
-			Console.WriteLine("The flashcard has been created successfully.");
-			SetState(new State(_stateContext));
-		}
-	}
+            _context.Flashcards.Add(flashcard);
+
+            _context.SaveChanges();
+
+            Console.WriteLine("The flashcard has been created successfully.");
+            SetState<CreateFlashcard>();
+        }
+    }
 }
